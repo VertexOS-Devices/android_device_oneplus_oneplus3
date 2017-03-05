210ecfa55cd556ea6d4fdfa2844c02b0fa0e1920
joshuous <joshuous@gmail.com>
2017-03-05 17:45:29 +0000

    op3: power: Don't set top-app boost to 50 on POWER_HINT_INTERACTION
    This is way too aggressive, and the value persists indefinitely.

--- power/power.c
+++ power/power.c
@@ -452,11 +452,11 @@ static void power_hint(struct power_module *module, power_hint_t hint,
 
             // Scheduler is EAS.
             if (is_eas_governor(governor)) {
-                // Setting the value of foreground schedtune boost to 50 and
+                // Setting the value of foreground schedtune boost to 50 (nope please don't do this)
                 // scaling_min_freq to 1100MHz.
                 int eas_interaction_resources[] = { MIN_FREQ_BIG_CORE_0, 1100, 
                                                     MIN_FREQ_LITTLE_CORE_0, 1100, 
-                                                    STOR_CLK_SCALE_DIS, 0x32, 
+                                                    //STOR_CLK_SCALE_DIS, 0x32, 
                                                     CPUBW_HWMON_MIN_FREQ, 0x33};
                 interaction(duration, sizeof(eas_interaction_resources)/sizeof(eas_interaction_resources[0]), eas_interaction_resources);
             } else { // Scheduler is HMP.
