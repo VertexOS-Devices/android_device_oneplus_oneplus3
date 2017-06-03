6cc36d49ea33411d1604e1a6068079cd6584c192
joshuous <joshuous@gmail.com>
2017-06-03 16:51:44 +0000

    op3: Enable dexpreopt for user and userdebug builds

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -173,7 +173,7 @@ SF_VSYNC_EVENT_PHASE_OFFSET_NS := 6000000
 
 # Enable dexpreopt to speed boot time
 ifeq ($(HOST_OS),linux)
-  ifeq ($(TARGET_BUILD_VARIANT),user)
+  ifneq ($(TARGET_BUILD_VARIANT),eng)
     ifeq ($(WITH_DEXPREOPT),)
       WITH_DEXPREOPT := true
       DONT_DEXPREOPT_PREBUILTS := true
