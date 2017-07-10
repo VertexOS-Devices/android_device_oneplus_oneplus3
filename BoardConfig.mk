0de25177c2ad770806a0b5c7501b06cf27f30c2d
joshuous <joshuous@gmail.com>
2017-07-10 18:13:55 +0000

    op3: Use SDCLANG
    Strangely, I get lots of app crashes if I don't use SDCLANG...
    
    Signed-off-by: joshuous <joshuous@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -37,6 +37,12 @@ TARGET_OTA_ASSERT_DEVICE := OnePlus3,oneplus3,OnePlus3T,oneplus3t
 # Use Snapdragon LLVM, if available
 TARGET_USE_SDCLANG := true
 
+ifneq ($(HOST_OS),darwin)
+    SDCLANG := true
+    SDCLANG_PATH := prebuilts/clang/linux-x86/host/sdclang-3.8/bin
+    SDCLANG_LTO_DEFS := device/qcom/common/sdllvm-lto-defs.mk
+endif
+
 # Bootloader
 TARGET_BOOTLOADER_BOARD_NAME := msm8996
 TARGET_NO_BOOTLOADER := true
