0970e2a4dcf67dd1bae82172cfdf908c3e6ed1d9
joshuous <joshuous@gmail.com>
2017-07-25 08:38:46 +0000

    op3: Use good old GCC 4.9 for comparison
    Signed-off-by: joshuous <joshuous@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -147,9 +147,9 @@ TARGET_HW_DISK_ENCRYPTION := true
 #TARGET_ROM_CUSTOM_TOOLCHAIN_AARCH64 := aarch64-linux-android-uber-5.x
 
 # Custom Kernel Toolchains
-TARGET_KERNEL_CUSTOM_TOOLCHAIN := aarch64-linux-gnu-6.x-kernel-linaro
-KERNEL_TOOLCHAIN := $(ANDROID_BUILD_TOP)/prebuilts/gcc/$(HOST_OS)-x86/aarch64/$(TARGET_KERNEL_CUSTOM_TOOLCHAIN)/bin
-KERNEL_TOOLCHAIN_PREFIX := aarch64-linux-gnu-
+#TARGET_KERNEL_CUSTOM_TOOLCHAIN := aarch64-linux-gnu-6.x-kernel-linaro
+#KERNEL_TOOLCHAIN := $(ANDROID_BUILD_TOP)/prebuilts/gcc/$(HOST_OS)-x86/aarch64/$(TARGET_KERNEL_CUSTOM_TOOLCHAIN)/bin
+#KERNEL_TOOLCHAIN_PREFIX := aarch64-linux-gnu-
 
 # Display
 BOARD_USES_ADRENO := true
